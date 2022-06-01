7e476c2912c039c25ef0b4d8b8a86f2c6b7267fe
shumer <infern333@gmail.com>
2022-06-01 19:53:43 +0300

    relocate

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 import logging
-
-import pandas as pd
-from flask import Flask
-from load_quotes import get_dataframe_from_csv
+from flask import Flask, render_template
 import os
 from settings import data_folder, file_name
-from sqlalchemy import delete
-
-
+import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -20,11 +15,7 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["DEBUG"] = True
 
-    from models.models import (
-        Quote,
-        get_quotes_for_market,
-        db
-    )
+    from models import db, get_quotes_from_db
 
     db.init_app(app)
 
@@ -38,21 +29,11 @@ def create_app():
 
     @app.route("/", methods=["GET"])
     def get_main_page():
-        file_path = os.path.join(data_folder, file_name)
-        quotes = pd.DataFrame()
-        try:
-            quotes = get_dataframe_from_csv(file_path=file_path)
-        except Exception as exc:
-            logger.error(f'Data loading error {exc}')
-
-        if not quotes.empty:
-            quotes.to_sql('quotes', db.engine, if_exists='replace')
-            db.session.commit()
-        return "ok"
-
-    @app.route("/quotes/<market>", methods=["GET"])
-    def get_quotes(market):
-        return get_quotes_for_market(market=market)
+        date, close, seasonality = get_quotes_from_db()
+        return render_template('index.html',
+                               dt=json.dumps(date),
+                               close=close,
+                               seasonality=seasonality)
 
     return app
 
@@ -60,3 +41,12 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run()
+
+    from models import db, get_quotes_from_csv,\
+        load_quotes_to_db, get_quotes_with_seasonality, get_quotes_from_db
+
+    # file_path = os.path.join(data_folder, file_name)
+    # quotes = get_quotes_from_csv(file_path=file_path)
+    # quotes_with_seasonality = get_quotes_with_seasonality(quotes=quotes)
+    # load_quotes_to_db(quotes=quotes_with_seasonality)
+
---
+++ app/models.py
@@ -0,0 +1,110 @@
+import logging
+from typing import Any, Dict
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import delete
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+db = SQLAlchemy()
+
+
+class Quote(db.Model):
+    __tablename__ = "quote"
+
+    market = db.Column(db.String(10), primary_key=True)
+    date = db.Column(db.Date, primary_key=True)
+    open = db.Column(db.Float, nullable=False)
+    high = db.Column(db.Float, nullable=False)
+    low = db.Column(db.Float, nullable=False)
+    close = db.Column(db.Float, nullable=False)
+    seasonality = db.Column(db.Float)
+    __table_args__ = (
+        db.UniqueConstraint("market", "date", name="unique_market_date"),
+    )
+
+    def __repr__(self):
+        return f"Quotes {self.market} {self.date} {self.close}"
+
+    def to_json(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+def get_quotes_from_csv(file_path: str):
+    try:
+        header_list = ["market", "date", "open", "high", "low", "close"]
+        quotes = pd.read_csv(file_path,
+                             parse_dates=False, delimiter=',', decimal='.',
+                             names=header_list)
+        quotes['date'] = pd.to_datetime(quotes['date'], format='%m/%d/%Y')
+    except Exception as e:
+        logger.error(f'Data loading error. {e}')
+        return 'Internal server error', 500
+
+    return quotes
+
+
+def load_quotes_to_db(quotes):
+    if not quotes.empty:
+        try:
+            market = quotes.iloc[0][0]
+            delete_quotes(market=market)
+            quotes.to_sql('quote', con=db.engine, if_exists='append', index=False)
+            db.session.commit()
+        except Exception as e:
+            logger.error(f'Cant load data to database. {e}')
+            return 'Internal server error', 500
+
+    return quotes
+
+
+def delete_quotes(market):
+    db.session.execute(delete(Quote).where(Quote.market == market))
+    db.session.commit()
+
+
+def get_seasonality(quotes):
+    quotes['delta'] = quotes.close - quotes.close.shift(1)
+    quotes.at[0, 'delta'] = 0
+    quotes['day_of_year'] = pd.DatetimeIndex(quotes.date).day_of_year
+    mean_delta = quotes.groupby('day_of_year')['delta'].mean()
+    cumulative_sum = mean_delta.cumsum()
+    seasonality = pd.DataFrame(cumulative_sum)
+    seasonality['delta'] = signal.detrend(seasonality['delta'])
+    # print(seasonality.to_string())
+    # seasonal.plot()
+    # plt.show()
+    return seasonality
+
+
+def get_quotes_with_seasonality(quotes):
+    seasonality = get_seasonality(quotes=quotes)
+    quotes['day_of_year'] = pd.DatetimeIndex(quotes.date).day_of_year
+    quotes = quotes.merge(seasonality, on='day_of_year', how='left')
+    quotes = quotes.drop(columns=['delta_x', 'day_of_year'])
+    quotes = quotes.rename(columns={'delta_y': 'seasonality'})
+    # print(quotes.to_string())
+
+    return quotes
+
+
+def get_quotes_from_db(market='KC-057'):
+    quotes = db.session.query(Quote.date, Quote.close, Quote.seasonality).\
+        filter(Quote.market == market).all()
+    date = list()
+    close = list()
+    seasonality = list()
+    i = 0
+    for quote in quotes:
+        date.append(str(quote.date))
+        close.append(quote.close)
+        seasonality.append(quote.seasonality * 10)
+        i += 1
+        if i == 300:
+            break
+
+    return date, close, seasonality
